c1e9d69ca2edfcc92d486713b376898026d3dd8b
Ashley Bunch <bunchley007@gmail.com>
2025-01-21 19:40:54 -0700

    added card buttons and styling

--- script.js
+++ script.js
@@ -11,6 +11,18 @@ function createCard() {
   const summary = document.createElement("p");
   summary.textContent = "stuff to describe project";
   newCard.appendChild(summary);
+  const cardShare = document.createElement("div");
+  cardShare.classList.add("button-share");
+  const star = document.createElement("i");
+  star.classList.add("fa-regular", "fa-star");
+  cardShare.appendChild(star);
+  const eye = document.createElement("i");
+  eye.classList.add("fa-regular", "fa-eye");
+  cardShare.appendChild(eye);
+  const link = document.createElement("i");
+  link.classList.add("fa-solid", "fa-share-nodes");
+  cardShare.appendChild(link);
+  newCard.appendChild(cardShare);
   return newCard;
 }
 
